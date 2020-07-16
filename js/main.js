76955021efa47490fbb2e346e8d33b0094eebc6d
Александр Якушин <sashock60@gmail.com>
2020-07-16 21:46:47 +0500

    task3

--- js/main.js
+++ js/main.js
@@ -38,82 +38,197 @@ var PHOTOS = [
 var PIN_WIDTH = 62;
 var PIN_HEIGHT = 22;
 
-var objects = [];
+var INTERVALS = {
+  locationX: {
+    min: 100,
+    max: 980
+  },
+  locationY: {
+    min: 130,
+    max: 630
+  },
+  price: {
+    min: 100,
+    max: 1000
+  },
+  types: {
+    min: 0,
+    max: 4
+  },
+  roomsAndGuests: {
+    min: 1,
+    max: 10
+  },
+  chekinAndCheckout: {
+    min: 0,
+    max: 3
+  }
+};
+
 
 function generateRandomNumberFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function generateRandomStringFromArray(array) {
+function generateRandomArray(array) {
   var randomArray = [];
   var randomCount = Math.ceil(Math.random() * array.length);
 
-  for (var i = 0; i < randomCount; i += 1) {
+  for (var i = 0; i < randomCount; i++) {
     randomArray.push(array[i]);
   }
 
-  return randomArray.toString();
+  return randomArray;
 }
 
-var createNewObjs = function (count) {
+var createNewPinsArray = function (count) {
+  var objects = [];
   for (var i = 0; i < count; i += 1) {
+    var locX = generateRandomNumberFromInterval(INTERVALS.locationX.min, INTERVALS.locationX.max) - PIN_WIDTH / 2;
+    var locY = generateRandomNumberFromInterval(INTERVALS.locationY.min, INTERVALS.locationY.max) - PIN_HEIGHT;
     objects.push({
       author: {avatar: 'img/avatars/user0' + (i + 1) + '.png'},
       offer: {
         title: TITLES[i],
-        address: [
-          generateRandomNumberFromInterval(100, 1000),
-          generateRandomNumberFromInterval(100, 1000),
-        ].toString(),
-        price: generateRandomNumberFromInterval(100, 1000),
-        type: TYPES[generateRandomNumberFromInterval(0, 4)],
-        rooms: generateRandomNumberFromInterval(0, 10),
-        guests: generateRandomNumberFromInterval(0, 10),
-        checkin: CHECKIN[generateRandomNumberFromInterval(0, 3)],
-        checkout: CHECKOUT[generateRandomNumberFromInterval(0, 3)],
-        features: generateRandomStringFromArray(FEATURES),
+        address: locX + ' ' + locY,
+        price: generateRandomNumberFromInterval(INTERVALS.price.min, INTERVALS.price.max),
+        type: TYPES[generateRandomNumberFromInterval(INTERVALS.types.min, INTERVALS.types.max)],
+        rooms: generateRandomNumberFromInterval(INTERVALS.roomsAndGuests.min, INTERVALS.roomsAndGuests.max),
+        guests: generateRandomNumberFromInterval(INTERVALS.roomsAndGuests.min, INTERVALS.roomsAndGuests.max),
+        checkin: CHECKIN[generateRandomNumberFromInterval(INTERVALS.chekinAndCheckout.min, INTERVALS.chekinAndCheckout.max)],
+        checkout: CHECKOUT[generateRandomNumberFromInterval(INTERVALS.chekinAndCheckout.min, INTERVALS.chekinAndCheckout.max)],
+        features: generateRandomArray(FEATURES),
         description: DESCRIPTONS[i],
-        photos: generateRandomStringFromArray(PHOTOS),
+        photos: generateRandomArray(PHOTOS),
       },
       location: {
-        x: generateRandomNumberFromInterval(100, 980) - PIN_WIDTH / 2,
-        y: generateRandomNumberFromInterval(130, 630) - PIN_HEIGHT,
+        x: locX,
+        y: locY,
       },
     });
   }
   return objects;
 };
 
-//вторая часть задания DOM
+// вторая часть задания DOM
 
 var pin = document.querySelector('.map__pins');
-var fragment = document.createDocumentFragment();
 var pinsArray = [];
-var objectsArray = createNewObjs(8);
+var objectsArray = createNewPinsArray(8);
 
-function createPinsFromObjects(objects) {
-  for (var i = 0; i < objects.length; i += 1) {
-    var newBtn = document.createElement('button');
-    var img = document.createElement('img');
-    newBtn.className = 'map__pin';
-    newBtn.style.left = objects[i].location.x + 'px';
-    newBtn.style.top = objects[i].location.y + 'px';
-    img.src = objects[i].author.avatar;
-    img.width = 50;
-    img.height = 50;
-    img.alt = objects[i].offer.title;
-    img.style.top = 0;
-    newBtn.appendChild(img);
-    pinsArray.push(newBtn);
-  }
+function createPinsFromArray(objectsPins) {
+  objectsPins.forEach(function (item) {
+    var templatePin = document.querySelector('#pin').content.querySelector('button');
+    var element = templatePin.cloneNode(true);
+    element.style.left = item.location.x + 'px';
+    element.style.top = item.location.y + 'px';
+    element.children[0].src = item.author.avatar;
+    element.children[0].alt = item.offer.title;
+    pinsArray.push(element);
+  });
 }
 
 function addedPinsInParentNode(pins) {
-  for (var i = 0; i < pins.length; i += 1) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < pins.length; i++) {
     fragment.appendChild(pins[i]);
   }
   pin.appendChild(fragment);
 }
 
-createPinsFromObjects(objectsArray);
+createPinsFromArray(objectsArray);
 addedPinsInParentNode(pinsArray);
+
+var popupsArray = [];
+var popup = document.querySelector('.map');
+
+function createPopup() {
+  var templatePopup = document.querySelector('#card').content.querySelector('.map__card, .popup');
+  var element = templatePopup.cloneNode(true);
+  var avatar = objectsArray[0].author.avatar;
+  var title = objectsArray[0].offer.title;
+  var address = objectsArray[0].offer.address;
+  var price = objectsArray[0].offer.price;
+  var type = objectsArray[0].offer.type;
+  var rooms = objectsArray[0].offer.rooms;
+  var guests = objectsArray[0].offer.guests;
+  var checkin = objectsArray[0].offer.checkin;
+  var checkout = objectsArray[0].offer.checkout;
+  var features = objectsArray[0].offer.features;
+  var description = objectsArray[0].offer.description;
+  var photos = objectsArray[0].offer.photos;
+
+  element.children[0].src = avatar;
+  element.children[2].textContent = title;
+  element.children[3].textContent = address;
+  element.children[4].textContent = price + ' ₽/ночь';
+  switch (type) {
+    case 'palace':
+      element.children[5].textContent = 'Дворец';
+      break;
+    case 'flat':
+      element.children[5].textContent = 'Квартира';
+      break;
+    case 'bungalo':
+      element.children[5].textContent = 'Бунгало';
+      break;
+    case 'house':
+      element.children[5].textContent = 'Дом';
+      break;
+  }
+  element.children[6].textContent = rooms + ' комнаты для ' + guests + ' гостей';
+  element.children[7].textContent = 'Заезд после ' + checkin + ' , выезд до ' + checkout;
+  var featuresArray = element.children[8].children;
+  for (var i = 0; i < featuresArray.length; i++) {
+    featuresArray[i].style.display = 'none';
+  }
+  features.forEach(function (item) {
+    switch (item) {
+      case 'wifi':
+        element.children[8].children[0].style.display = 'inline-block';
+        break;
+      case 'dishwasher':
+        element.children[8].children[1].style.display = 'inline-block';
+        break;
+      case 'parking':
+        element.children[8].children[2].style.display = 'inline-block';
+        break;
+      case 'washer':
+        element.children[8].children[3].style.display = 'inline-block';
+        break;
+      case 'elevator':
+        element.children[8].children[4].style.display = 'inline-block';
+        break;
+      case 'conditioner':
+        element.children[8].children[5].style.display = 'inline-block';
+        break;
+    }
+  });
+  element.children[9].textContent = description;
+  for (var j = 0; j < objectsArray[0].offer.photos.length; j++) {
+    if (j === 0) {
+      element.children[10].children[0].src = photos[0] + '';
+    } else {
+      var newImg = document.createElement('img');
+      newImg.src = photos[j] + '';
+      newImg.className = 'popup__photo';
+      newImg.style.width = '45px';
+      newImg.style.height = '40px';
+      newImg.alt = 'Фотография жилья';
+      element.children[10].appendChild(newImg);
+    }
+  }
+  popupsArray.push(element);
+}
+
+function addedPopupInParentNode(popups) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < popups.length; i++) {
+    fragment.appendChild(popups[i]);
+  }
+  popup.appendChild(fragment);
+}
+
+
+createPopup();
+addedPopupInParentNode(popupsArray);
